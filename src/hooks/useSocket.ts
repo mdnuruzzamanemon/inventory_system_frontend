9ec2bcb04c2bd633fd5c6512ee38bcc1100c046e
mdnuruzzamanemon <mdnuruzzamanemon@gmail.com>
2026-07-11 01:05:46 +0600

    api url import from env

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { io, type Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:4000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000';
 
 interface SocketCallbacks {
   onStockUpdate: (productId: string, availableStock: number) => void;
